a023480c01b09f67702398f423d5305c71d7a757
Răzvan-Andrei Timofte <74573568+razandtim@users.noreply.github.com>
2026-01-23 09:07:43 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 // WiFi configuration
-pub const WIFI_SSID: &str = "treisaispe";
-pub const WIFI_PASSWORD: &str = "raztim1805";
+pub const WIFI_SSID: &str = "YOUR_WIFI_NAME";
+pub const WIFI_PASSWORD: &str = "YOUR_WIFI_PASSWORD";
 
 // Web server port
 pub const HTTP_PORT: u16 = 80;
